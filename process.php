e0351b41c012d3e6e5a70c679afb90ad10ecacef
edu <edwinkipchumba@autlook.com>
2023-02-15 11:58:10 +0300

    first commit

---
+++ process.php
@@ -0,0 +1,8 @@
+<?php
+    $svc_no=$_POST["svc_no"];
+    $rank=$_POST["rank"];
+    $name=$_POST["name"];
+     echo"centre";
+        echo"service no:" .$scv_no. "<br>rank:" .$rank."<br>name:" .$name;
+        echo"</centre>"
+    ?>
